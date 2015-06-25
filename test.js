0497a41bfffc2dc3e7e32536865e10552207557f
mattdesl <dave.des@gmail.com>
2015-06-25 17:05:49 -0400

    bam!

---
+++ test.js
@@ -0,0 +1,46 @@
+var inject = require('./')
+var test = require('tape')
+var tokenize = require('glsl-tokenizer')
+var stringify = require('glsl-token-string')
+
+test("get first token where it's safe to define values", function (t) {
+  var injected = { data: '#define blah', type: 'preprocessor' }
+
+  var shader = tokenize('#define foo\nvoid main(){}')
+  t.equal(stringify(inject(shader, injected)), 
+    '#define blah\n#define foo\nvoid main(){}')
+
+  shader = tokenize('void main(){}')
+  t.equal(stringify(inject(shader, injected)), 
+    '#define blah\nvoid main(){}')
+
+  shader = tokenize('')
+  t.equal(stringify(inject(shader, injected)), 
+    '#define blah\n')
+
+  shader = tokenize('#version 330')
+  t.equal(stringify(inject(shader, injected)), 
+    '#version 330\n#define blah\n')
+
+  shader = tokenize('#version 330\n\nvoid main(){}')
+  t.equal(stringify(inject(shader, injected)), 
+    '#version 330\n#define blah\n\nvoid main(){}')
+
+  shader = tokenize('#version 330//commentt\n/*comment*/\n#extension blah\nuniform vec4 foo;')
+  t.equal(stringify(inject(shader, injected)), 
+    '#version 330//commentt\n/*comment*/\n#extension blah\n#define blah\nuniform vec4 foo;')
+
+  shader = tokenize('#version 330//commentt\n/*comment*/\n#extension blah\nuniform vec4 foo;')
+  t.equal(stringify(inject(shader, [
+    { type: 'whitespace', data: '\n' },
+    { type: 'keyword', data: 'out' },
+    { type: 'whitespace', data: ' ' },
+    { type: 'keyword', data: 'float' },
+    { type: 'whitespace', data: ' ' },
+    { type: 'ident', data: 'foobar' },
+    { type: 'operator', data: ';' },
+    { type: 'whitespace', data: '\n' }
+  ])), 
+    '#version 330//commentt\n/*comment*/\n#extension blah\n\nout float foobar;\n\nuniform vec4 foo;')
+  t.end()
+})
